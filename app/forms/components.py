afb4eb66ff6e41fc80b6e5caaa967ab370ea57b2
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-11-15 00:09:17 +0100

    date request component initialized for further expansion

---
+++ app/forms/components.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import DateField, StringField, SubmitField
+from wtforms.validators import DataRequired, InputRequired
+
+class DateRequestForm(FlaskForm):
+    message = StringField('Optional text attached to accept/reject/reschedule', [InputRequired(message="Message can't be empty")])
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def __init__(self, message_label_text=None, given_date=None, *args, **kwargs):
+        super(DateRequestForm, self).__init__(*args, **kwargs)
+        if message_label_text:
+            self.message.label.text = message_label_text
+        if given_date:
+            self.date = StringField(label=given_date, render_kw={"readonly": True})
