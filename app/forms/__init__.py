e4d78526a0cba18988ff14a4883550721b3818de
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-11-15 00:29:07 +0100

    date request component initialized for further expansion (#13)
    * date request component initialized for further expansion
    
    * black + isort
    
    ---------
    
    Co-authored-by: janek_szyndlarewicz <jan.szyndlarewicz@gmail.com>

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,4 +1,5 @@
 from app.forms.account_manager_form import AccountManagerForm
+from app.forms.components import DateRequestForm
 from app.forms.login_form import LoginForm
 from app.forms.profile_manager_form import ProfileManagerForm
 from app.forms.register_form import RegisterForm
