a33a3225909db889542a8127c774c8eceb673823
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-11-15 00:18:09 +0100

    sorted black

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,4 +1,4 @@
+from .components import DateRequestForm
 from .login_form import LoginForm
 from .profile_manager_form import ProfileManagerForm
 from .register_form import RegisterForm
-from .components import DateRequestForm
--- app/views/find_page.py
+++ app/views/find_page.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
+from app.forms import DateRequestForm
+
 find_page_bp = Blueprint("find_page_bp", __name__)
 
 
 @find_page_bp.route("/find-page")
 @login_required
 def find_page():
-    return render_template("find_page.html")
+    date_request_form = DateRequestForm(
+        message_label_text="Optional text attached to accept/reject/reschedule", given_date="2021-01-01"
+    )
+    is_requesting = True
+    return render_template("find_page.html", form=date_request_form, is_requesting=is_requesting)
