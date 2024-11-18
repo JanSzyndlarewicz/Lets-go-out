0df21301ae8767dc92398ef21fd0941a8b6d9f02
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-11-18 01:47:21 +0100

    improve frontend daterequest

--- app/forms/components.py
+++ app/forms/components.py
@@ -2,13 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, SubmitField
 from wtforms.validators import DataRequired, InputRequired
 
-
 class DateRequestForm(FlaskForm):
     message = StringField(
         "Optional text attached to accept/reject/reschedule", [InputRequired(message="Message can't be empty")]
     )
     date = DateField("Date", format="%Y-%m-%d", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    # submit_reject = SubmitField("Reject", widget=LabeledIconSubmitWidget())
+    # submit_ignore = SubmitField("Ignore", widget=LabeledIconSubmitWidget())
+    # submit_reschedule = SubmitField("Reschedule", widget=LabeledIconSubmitWidget())
+    # submit_accept = SubmitField("Accept", widget=LabeledIconSubmitWidget())
+    # submit_invite = SubmitField("Invite", widget=LabeledIconSubmitWidget())
 
     def __init__(self, message_label_text=None, given_date=None, *args, **kwargs):
         super(DateRequestForm, self).__init__(*args, **kwargs)
