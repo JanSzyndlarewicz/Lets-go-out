e78f1f9c0708624607d4495452110740b5ba4d76
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-11-22 18:39:13 +0100

    mappings corrected to the same standard

--- app/models/interest.py
+++ app/models/interest.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Table
+from sqlalchemy import ForeignKey, String, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.associations import ProfileInterestAssociation
@@ -7,8 +7,11 @@ from app.models.database import db
 
 class Interest(db.Model):
     __tablename__ = "interest"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False,
+    )
     profiles: Mapped[list["Profile"]] = relationship(
         "Profile",
         secondary=ProfileInterestAssociation.__table__,
