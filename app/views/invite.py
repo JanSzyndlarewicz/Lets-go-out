6eb1cd9461273835bf53678f0225d654a557c9bf
I <michal_wlodarczykf4@wp.pl>
2024-11-22 11:27:31 +0100

    Removed debugging alerts.

--- app/views/invite.py
+++ app/views/invite.py
@@ -17,6 +17,8 @@ invite_bp = Blueprint("invite_bp", __name__)
 @confirmed_required
 def invite(user_id : int):
     form = DateProposalForm()
+    #we will also have to validate whether the date is in the future, and whether there are tables available
+    #we can probaby write custom wtf validators for that
     if form.validate_on_submit():
         user = db.get_or_404(User, user_id)
         proposal = DateProposal(proposer_id=current_user.id, recipient_id=user_id, date=form.date.data, proposal_message=form.message.data)
