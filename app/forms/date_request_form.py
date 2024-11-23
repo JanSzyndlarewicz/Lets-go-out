92bf9fc3010238d1cc0ed5a347e37fc6baa35ddd
I <michal_wlodarczykf4@wp.pl>
2024-11-23 15:52:34 +0100

    Added validator to check if a date is not in the past, some adjustments to forms

--- app/forms/date_request_form.py
+++ app/forms/date_request_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import DateField, StringField
 from wtforms.validators import DataRequired, InputRequired
+from app.forms.validators import is_not_past
 
 
 class DateRequestForm(FlaskForm):
@@ -8,7 +9,7 @@ class DateRequestForm(FlaskForm):
         "Optional text attached to accept/reject/reschedule",
         validators=[InputRequired(message="Message can't be empty.")],
     )
-    date = DateField("Date", format="%Y-%m-%d", validators=[DataRequired()])
+    date = DateField("Date", format="%Y-%m-%d", validators=[DataRequired(), is_not_past])
 
     def __init__(self, message_label_text=True, given_date=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,4 +25,4 @@ class DateRequestForm(FlaskForm):
                 "value": given_date.strftime("%Y-%m-%d"),
             }  # Makes the field readonly
             self.date.data = given_date  # Pre-fill the field with the given date
-        self.process()
+        #self.process()
--- app/views/invite.py
+++ app/views/invite.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_login import current_user
 
 from app import User, db
-from app.forms import DateProposalForm
+from app.forms import DateRequestForm, DateProposalForm
 from app.models import DateProposal
 from app.views.auth import confirmed_required
 
@@ -12,10 +12,12 @@ invite_bp = Blueprint("invite_bp", __name__)
 @invite_bp.route("/invite/<int:user_id>", methods=["POST"])
 @confirmed_required
 def invite(user_id: int):
-    form = DateProposalForm()
+    form = DateRequestForm()
     # we will also have to validate whether the date is in the future, and whether there are tables available
     # we can probaby write custom wtf validators for that
+    print("invite")
     if form.validate_on_submit():
+        print("przeszło")
         user = db.get_or_404(User, user_id)
         proposal = DateProposal(
             proposer_id=current_user.id, recipient_id=user_id, date=form.date.data, proposal_message=form.message.data
@@ -23,4 +25,6 @@ def invite(user_id: int):
         db.session.add(proposal)
         db.session.commit()
         return "true"
+    print("nie przeszło")
+    print(form.errors)
     return form.errors
