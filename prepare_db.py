06c9c80f183c782b1561b62dfd2031814f3d7213
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-12-02 02:21:47 +0100

    minor findpage improvments

--- prepare_db.py
+++ prepare_db.py
@@ -81,7 +81,7 @@ with app.app_context():
                 "max_age": 5,
             },
         }
-        for i in range(1, 16)
+        for i in range(1, 100)
     ]
 
     for user_data in new_users:
