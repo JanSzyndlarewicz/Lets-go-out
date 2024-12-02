615f95f58efa37c01e031d0b1fe107e00377997e
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-12-02 04:01:26 +0100

    improved styling and all endpoints connected

--- app/views/matches_page.py
+++ app/views/matches_page.py
@@ -16,7 +16,7 @@ def matches_page():
 @confirmed_required
 def matches_page_proposed():
     proposed_and_ignored = current_user.profile.proposed_and_ignored_proposals_by_self
-    
+    print(proposed_and_ignored)
     return render_template(
         "main/matches_page.html", 
         proposals_data=proposed_and_ignored, 
@@ -26,20 +26,35 @@ def matches_page_proposed():
 @matches_page_bp.route("/matches-page/accepted")
 @confirmed_required
 def matches_page_accepted():
+    proposed_and_ignored = current_user.profile.accepted_proposals_by_self
     
-    return render_template("main/matches_page.html")
+    return render_template(
+        "main/matches_page.html", 
+        proposals_data=proposed_and_ignored, 
+        redirect_buttons_set_data=redirect_buttons_set_data()
+        )
 
 @matches_page_bp.route("/matches-page/rejected")
 @confirmed_required
 def matches_page_rejected():
+    proposed_and_ignored = current_user.profile.rejected_proposals_by_self
     
-    return render_template("main/matches_page.html")
+    return render_template(
+        "main/matches_page.html", 
+        proposals_data=proposed_and_ignored, 
+        redirect_buttons_set_data=redirect_buttons_set_data()
+        )
 
 @matches_page_bp.route("/matches-page/reschedule")
 @confirmed_required
 def matches_page_reschedule():
+    proposed_and_ignored = current_user.profile.reschedule_proposals_by_self
     
-    return render_template("main/matches_page.html")
+    return render_template(
+        "main/matches_page.html", 
+        proposals_data=proposed_and_ignored, 
+        redirect_buttons_set_data=redirect_buttons_set_data()
+        )
 
 def redirect_buttons_set_data():
     return [{"url": url_for("matches_page_bp.matches_page_proposed"), "text": "proposed"}, 
