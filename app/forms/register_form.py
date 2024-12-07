89cb3aa6f34475d239872de97329aa0cea52a4ac
I <michal_wlodarczykf4@wp.pl>
2024-12-07 14:07:19 +0100

    Added validators to check uniqueness of username and email

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, StringField, SubmitField
 from wtforms.validators import Email, EqualTo, InputRequired, Length
 
+from app.forms.validators import email_unique,username_unique
+
 
 class RegisterForm(FlaskForm):
-    username = StringField("User name", [InputRequired(message="Please input an username.")])
+    username = StringField("User name", [InputRequired(message="Please input an username."), username_unique])
     # minimal password length set to 1 for convenience of testing; set to something like 8 in final version
     password = PasswordField(
         "Password",
@@ -28,6 +30,7 @@ class RegisterForm(FlaskForm):
         [
             InputRequired(message="Email address is required."),
             Email(message="Please provide a correct email address."),
+            email_unique
         ],
     )
     submit = SubmitField("Next")
--- app/forms/validators.py
+++ app/forms/validators.py
@@ -4,15 +4,23 @@ from wtforms.validators import ValidationError
 
 from app.models.tables_config import TablesConfig
 
+from app import db, User
+
 
 def is_not_past(form, field):
     if field.data < date.today():
         raise ValidationError("You cannot choose a date in the past.")
 
 def is_any_table_available_for_date(form, field):
-    print(type(field.data))
-    print(not TablesConfig.get_is_any_available(field.data))
-    print(TablesConfig.get_avaliable_tables_for_day(field.data))
-    print(TablesConfig.get_max_tables())
     if not TablesConfig.get_is_any_available(field.data):
         raise ValidationError("No tables available for chosen date")
+    
+def username_unique(form, field):
+    user = User.query.filter_by(username=field.data).first()
+    if user is not None:
+        raise ValidationError("This username is already taken.")
+    
+def email_unique(form, field):
+    user = User.query.filter_by(email=field.data).first()
+    if user is not None:
+        raise ValidationError("This email address is already taken.")
